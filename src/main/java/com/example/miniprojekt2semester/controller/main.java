7ec6ce7a8e23ca00efb505b20a3d03bc67bb045e
Healsu <timmie2500@gmail.com>
2022-03-30 08:54:32 +0200

    update test

---
+++ src/main/java/com/example/miniprojekt2semester/controller/main.java
@@ -0,0 +1,14 @@
+package com.example.miniprojekt2semester.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class main {
+
+    @GetMapping("/")
+    public String mainpage(){
+
+        return null;
+    }
+}
