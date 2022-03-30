9bf3c5889d6cf9f8c43ae4a026f7cfde529ac5f9
samuelran <samuelbesserransby@gmail.com>
2022-03-30 10:15:49 +0200

    objekter :D

---
+++ src/main/java/models/wishList.java
@@ -0,0 +1,37 @@
+package models;
+
+public class wishList {
+    private int wishListID;
+    private String wishListName;
+    private int userID;
+
+    public wishList(int wishListID, String wishListName, int userID) {
+        this.wishListID = wishListID;
+        this.wishListName = wishListName;
+        this.userID = userID;
+    }
+
+    public int getWishListID() {
+        return wishListID;
+    }
+
+    public void setWishListID(int wishListID) {
+        this.wishListID = wishListID;
+    }
+
+    public String getWishListName() {
+        return wishListName;
+    }
+
+    public void setWishListName(String wishListName) {
+        this.wishListName = wishListName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
