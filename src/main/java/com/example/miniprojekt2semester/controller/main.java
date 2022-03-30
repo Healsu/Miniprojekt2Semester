1033908bdc00c0601a7785a4e0d904373660b5df
JimmyKristensen <siswanto@live.dk>
2022-03-30 09:17:15 +0200

    index and css

--- src/main/java/com/example/miniprojekt2semester/controller/main.java
+++ src/main/java/com/example/miniprojekt2semester/controller/main.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class main {
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String mainpage(){
-
-        return null;
+        return "index";
     }
 }
