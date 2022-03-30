84d66790329808403864e2b6f27d62340e4ec710
Healsu <timmie2500@gmail.com>
2022-03-30 08:50:58 +0200

    Initial commit

---
+++ src/main/java/com/example/miniprojekt2semester/Miniprojekt2SemesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojekt2semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojekt2SemesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miniprojekt2SemesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniprojekt2semester/Miniprojekt2SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojekt2semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojekt2SemesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
